c240571cac2784980416d2a0cd7a4d81a11fe875
Suvichan Prasongphonchai <acspdoctoraod@gmail.com>
2018-04-09 14:29:30 +0700

    Test hello package

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+class ThaiSmartcardReader extends React.Component {
+  render() {
+    return (
+      <div>Hello, Thai smartcard reader.</div>
+    );
+  }
+}
+export default ThaiSmartcardReader;
